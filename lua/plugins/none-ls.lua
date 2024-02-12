23c95b27823c174918b5e8d2e9d2b73d39b7f10c
Dyson <dysonsphere042@proton.me>
2024-02-11 19:14:04 -0800

    initial commit

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,18 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.formatting.autoflake,
+        null_ls.builtins.formatting.rustfmt,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
