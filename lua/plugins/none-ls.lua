75055966fc06cef4a428e1201a8fc313829eb9af
Dyson <dysonsphere042@proton.me>
2024-02-19 20:56:26 -0800

    add java support

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,6 +10,8 @@ return {
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.formatting.autoflake,
         null_ls.builtins.formatting.rustfmt,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.diagnostics.checkstyle,
       },
     })
 
